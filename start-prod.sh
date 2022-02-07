ec2f15ee30fe068518935e6f5108d402d4cb3a0e
doog33k <ecourcier@gmail.com>
2022-02-07 14:48:26 +0100

    prod files

--- start-prod.sh
+++ start-prod.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 app="lostark.engraving"
+<<<<<<< HEAD
 docker build -t ${app} -f Dockerfile-prod .
+=======
+docker build -t ${app} -f Dockerfile .
+>>>>>>> 7dd5a3f (prod files)
 docker run -d -p 6663:80 \
   --restart unless-stopped \
   --name=${app} \
