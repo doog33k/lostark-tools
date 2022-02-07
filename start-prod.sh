03b400f5506bcd8c75b3ea613e2b554ed8fd690c
Doog33k <ecourcier@gmail.com>
2022-02-07 10:02:20 +0100

    added filter for maxroll and Skorp

--- start-prod.sh
+++ start-prod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 app="lostark.engraving"
-sudo docker rm -f ${app}
-sudo docker rmi -f ${app}
-docker build -t plc402/${app} .
-docker run -d -p 5000:80 \
+docker build -t ${app} -f Dockerfile-prod .
+docker run -d -p 6663:80 \
+  --restart unless-stopped \
   --name=${app} \
   -v $PWD:/app \
   -v $PWD/app/static:/var/www/app/static plc402/${app}
